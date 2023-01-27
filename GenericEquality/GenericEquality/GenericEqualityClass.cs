bbe098a281757d8fb69d7898324e5f521f9666f6
Arthur Prado De Fazio <Arthur_Prado_De_Fazio@MacBook-Pro-Arthur-Prado-De-Fazio.local>
2023-01-27 12:22:13 -0300

    Add base files

---
+++ GenericEquality/GenericEquality/GenericEqualityClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GenericEquality
+{
+	public static class GenericEqualityClass
+	{
+		public static bool Equals<T>(T one, T other) where T: IComparable
+		{
+			return one.CompareTo(other) == 0;
+		}
+	}
+
+}
+
---
+++ GenericEquality/GenericEquality/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericEquality;
+class Program
+{
+    public static bool Equals(int m, int n)
+    {
+        return m == n;
+    }
+
+    public static bool Equals(string s, string t)
+    {
+        return s == t;
+    }
+
+    public static bool Equals(double x, double y)
+    {
+        return x == y;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Usando métodos com sobrecarga");
+        Console.WriteLine($"1.2 é igual a 3.1? {(Equals(1.2, 3.1) ? "Sim" : "Nao")}");
+        Console.WriteLine($"3 é igual a 3? {(Equals(3, 3) ? "Sim" : "Nao")}");
+        Console.WriteLine($"A string oi é igual a oi? {(Equals("oi", "oi") ? "Sim" : "Nao")}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Usando método genérico");
+        Console.WriteLine($"1.2 é igual a 1.2? {(GenericEqualityClass.Equals<double>(1.2, 1.2) ? "Sim" : "Nao")}");
+        Console.WriteLine($"3 é igual a 9? {(GenericEqualityClass.Equals<int>(3, 9) ? "Sim" : "Nao")}");
+        Console.WriteLine($"A string oi é igual a olá? {(Equals("oi", "olá") ? "Sim" : "Nao")}");
+
+    }
+}
+
